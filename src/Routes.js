a8574791e0737565965e55fc61dd0722ccd63a0b
Pablo Ch <pablo.chamba@crifa.com>
2022-11-15 13:08:50 -0500

    AF-1344 / add jwt-decode, ForgotPassword route

--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 
+/** Components **/
 /** Sign in **/
 import SignIn from './pages/signIn/SignIn'
+import ForgotPassword from './pages/signIn/ForgotPassword'
+
+/** Redux **/
+import { useSelector } from 'react-redux'
 
 const Routes = () => {
+  const token = useSelector(state => state.auth.token)
+  const user = useSelector(state => state.auth.user)
+
+  if (token && user) {
+    return (
+          <BrowserRouter>
+              <Switch>
+                  <Route exact path="/">
+                  </Route>
+              </Switch>
+          </BrowserRouter>
+    )
+  }
   return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to={'/sign-in'}/>
-          </Route>
-          <Route exact path="/sign-in">
-            <SignIn/>
-          </Route>
-        </Switch>
-      </BrowserRouter>)
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Redirect to={'/sign-in'}/>
+                </Route>
+                <Route exact path="/forgot-password">
+                    <ForgotPassword />
+                </Route>
+                <Route exact path="/sign-in">
+                    <SignIn/>
+                </Route>
+            </Switch>
+        </BrowserRouter>)
 }
 
 export default Routes
