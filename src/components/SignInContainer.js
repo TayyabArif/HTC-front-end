3c4f36d3f64f16e301b2f5acc10ce72be88178c7
Pablo Ch <pablo.chamba@crifa.com>
2022-12-16 16:11:09 -0500

    AF-1441 / fix

--- src/components/SignInContainer.js
+++ src/components/SignInContainer.js
@@ -43,7 +43,7 @@ const useStyles = makeStyles((theme) => ({
   footerContainer: {
     bottom: 30,
     position: 'relative',
-    [theme.breakpoints.up('lg')]: {
+    [theme.breakpoints.up('xl')]: {
       position: 'absolute'
     },
     '& img': {
