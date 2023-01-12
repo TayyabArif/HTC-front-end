053db319535247a630b01173ca78b06bf8903449
Martín Alarcón <martin.alarcon@crifa.com>
2023-01-12 16:32:37 -0500

    AD-1511

--- src/components/locations/map/LocationInfoCard.js
+++ src/components/locations/map/LocationInfoCard.js
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next'
 // Components
 import { Box, Card, Typography, Rating, Collapse, CardContent } from '@mui/material'
 import { ArrowDropDownRounded, ArrowDropUpRounded } from '@mui/icons-material'
-import { PhotoList } from './PhotoList'
+// import { PhotoList } from './PhotoList'
 
 /** Redux **/
 import { useSelector } from 'react-redux'
@@ -13,12 +13,10 @@ import { useSelector } from 'react-redux'
 import { locationInfoCardStyles } from '../../../styles/classes/LocationsClasses'
 
 // hardcoded photos
-const photosData = [
+/* const photosData = [
   {
     img: 'https://www.investopedia.com/thmb/6tBnqfHCJ_GHn_kcw6jGf2tMPDE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Walmart_exterior-8db53b3ec5c442f0a343fe01e6640090.jpg',
-    title: 'Store 1',
-    rows: 2,
-    cols: 2
+    title: 'Store 1'
   },
   {
     img: 'https://www.big-box.com/wp-content/uploads/2017/02/big-box-store-aisle-300x200.jpg',
@@ -36,7 +34,7 @@ const photosData = [
     img: 'https://www.joneslanglasalle.com.cn/images/apac/articles/jll-why-bigbox-retail-stores-are-turning-into-warehouses-social-1200x628.jpg',
     title: 'Store 5'
   }
-]
+] */
 
 export const LocationInfoCard = (props) => {
   const classes = locationInfoCardStyles()
@@ -55,14 +53,15 @@ export const LocationInfoCard = (props) => {
               : <ArrowDropUpRounded fontSize='small' className={classes.arrowDown} onClick={() => setExpanded(!expanded)}/>}
         </Box>
         <Box display="flex">
-            <Typography className={classes.ratingLabel} >{4.2}</Typography>
-            <Rating classes={{ root: classes.rating }} className={classes.rating} size='small' value={4} readOnly />
-            <Typography className={classes.ratingLabel} >{'(1.1K)'}</Typography>
+            <Typography className={classes.ratingLabel} >{props.info.rating}</Typography>
+            <Rating classes={{ root: classes.rating }} className={classes.rating} size='small' value={props.info.rating} readOnly />
+            <Typography className={classes.ratingLabel} >{`(${props.info.user_ratings_total})`}</Typography>
         </Box>
-        <Typography className={classes.locationDescription}>{'$ - Home improvement store in Little Hassy, Nevada'}</Typography>
+        <Typography className={classes.locationDescription}>{`$ - ${props.info.name}`}</Typography>
         <Collapse in={expanded} timeout="auto" unmountOnExit>
           <CardContent className={classes.cardContent}>
-            <PhotoList photos={photosData ?? []}/>
+            {/* <PhotoList photos={photosData ?? []}/> */}
+            <a href="https://maps.google.com/maps/contrib/104790014239029387558\">Museum of Contemporary Art Australia</a>
             <Box mb="6px" display="flex">
               <Typography className={classes.fieldContent}><Typography display="inline" className={classes.fieldLabel}>{t('locations.info_card.located')}:&nbsp;</Typography>{'Marketplace at Little Hassy'}</Typography>
             </Box>
