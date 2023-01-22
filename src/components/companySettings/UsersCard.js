93de8e5bda5c80b7ff9b2fb73a62164e43c4967e
Pablo Ch <pablo.chamba@crifa.com>
2023-01-21 22:13:13 -0500

    AF-1537 / capitalize pill

--- src/components/companySettings/UsersCard.js
+++ src/components/companySettings/UsersCard.js
@@ -151,7 +151,7 @@ export const UsersCard = props => {
                       roleOptions && user.roles && user.roles !== 'no_value'
                         ? roleOptions[user.roles]
                         : t('company_settings.mobile_only'),
-                      user.status
+                      user.status?.charAt(0).toUpperCase() + user.status?.slice(1)
                     ].filter(
                       item => typeof item !== 'undefined' && item !== '' && item
                     )}
