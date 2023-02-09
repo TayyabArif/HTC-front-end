25cac5b8b8e84cbd26f3b738333e63e6a32f08e3
Pablo Ch <pablo.chamba@crifa.com>
2023-02-08 19:03:00 -0500

    AF-1567 / fix fontawesome icons

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import './i18n'
 import App from './App'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor, store } from './store/index'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { fal } from '@fortawesome/pro-light-svg-icons'
+import { far } from '@fortawesome/pro-regular-svg-icons'
+import { fas } from '@fortawesome/pro-solid-svg-icons'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
+library.add(fal, fas, far)
+
 root.render(<Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
         <App/>
