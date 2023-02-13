7d5499e29612dbc5befa60f0e13b0b0371219409
Pablo Ch <pablo.chamba@crifa.com>
2023-02-13 14:27:51 -0500

    AF-1676 / validating Locations Listings field that is empty should not be displayed.

--- src/components/locations/WorkOrderCard.js
+++ src/components/locations/WorkOrderCard.js
@@ -59,32 +59,32 @@ export const WorkOrderCard = (props) => {
           </Box>
           <Box display="flex" width="100%">
             <Box flex={5}>
-              <Typography className={classes.woNumber} display="inline" align='left'>
+              {(info?.customer_po && info?.customer_po !== '') && <Typography className={classes.woNumber} display="inline" align='left'>
                 WO# {info?.customer_po}
-              </Typography>
-              <Typography marginBottom={0.3} className={classes.clientTracking} align='left'>
+              </Typography>}
+              {(info?.client_tracking_number && info?.client_tracking_number) && <Typography marginBottom={0.3} className={classes.clientTracking} align='left'>
                 {t('locations.work_orders.tracking')}# {info?.client_tracking_number}
-              </Typography>
-              <Typography marginBottom={0.3} className={classes.woType} align='left'>
-                {info?.call_type && info?.call_type !== '' ? info?.call_type : t('locations.work_orders.no_type')}
-              </Typography>
-              <Typography marginBottom={0.3} className={classes.startLabel} align='left'>
+              </Typography>}
+              {(info?.call_type && info?.call_type !== '') && <Typography marginBottom={0.3} className={classes.woType} align='left'>
+                {info?.call_type}
+              </Typography>}
+              {(info?.open_date && info?.open_date !== '') && <Typography marginBottom={0.3} className={classes.startLabel} align='left'>
                 {t('locations.work_orders.start')} {info?.open_date ? moment.unix(info.open_date).format('DD/MM/YY hh:mm a') : ''}
-              </Typography>
-              <Typography marginBottom={0.3} className={classes.woType} align='left'>
+              </Typography>}
+              {getServicesLabel() !== '' && <Typography marginBottom={0.3} className={classes.woType} align='left'>
                 {t('locations.work_orders.service_details')}: {getServicesLabel()}
-              </Typography>
+              </Typography>}
             </Box>
             <Box flex={3} pr={1}>
               <Typography marginBottom={0.3} className={classes.woStatus} align='left'>
                 {t(`work_orders.wo_states.${getWOstatus(info)}`)}
               </Typography>
-              <Typography marginBottom={0.3} className={classes.priority} align='left'>
+              {(info?.priority && info?.priority !== '') && <Typography marginBottom={0.3} className={classes.priority} align='left'>
                 {info?.priority}
-              </Typography>
-              <Typography marginBottom={0.3} className={classes.endLabel} align='left'>
+              </Typography>}
+              {(info?.expiration_date && info?.expiration_date !== '') && <Typography marginBottom={0.3} className={classes.endLabel} align='left'>
                 {t('locations.work_orders.end')} {info?.expiration_date ? moment.unix(info.expiration_date).format('DD/MM/YY hh:mm a') : ''}
-              </Typography>
+              </Typography>}
             </Box>
           </Box>
         </Paper>
