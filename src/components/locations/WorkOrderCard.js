4f02b7c823e9dbac96a50c65c738102e1214e738
Pablo Ch <pablo.chamba@crifa.com>
2023-02-17 10:37:58 -0500

    AF-1676 / locations work order card put a space when there is no data

--- src/components/locations/WorkOrderCard.js
+++ src/components/locations/WorkOrderCard.js
@@ -59,21 +59,21 @@ export const WorkOrderCard = (props) => {
           </Box>
           <Box display="flex" width="100%">
             <Box flex={5}>
-              {(info?.customer_po && info?.customer_po !== '') && <Typography className={classes.woNumber} display="inline" align='left'>
-                WO# {info?.customer_po}
-              </Typography>}
-              {(info?.client_tracking_number && info?.client_tracking_number) && <Typography marginBottom={0.3} className={classes.clientTracking} align='left'>
-                {t('locations.work_orders.tracking')}# {info?.client_tracking_number}
-              </Typography>}
-              {(info?.call_type && info?.call_type !== '') && <Typography marginBottom={0.3} className={classes.woType} align='left'>
-                {info?.call_type}
-              </Typography>}
-              {(info?.open_date && info?.open_date !== '') && <Typography marginBottom={0.3} className={classes.startLabel} align='left'>
-                {t('locations.work_orders.start')} {info?.open_date ? moment.unix(info.open_date).format('DD/MM/YY hh:mm a') : ''}
-              </Typography>}
-              {getServicesLabel() !== '' && <Typography marginBottom={0.3} className={classes.woType} align='left'>
-                {t('locations.work_orders.service_details')}: {getServicesLabel()}
-              </Typography>}
+              <Typography className={classes.woNumber} display="inline" align='left'>
+                {(info?.customer_po && info?.customer_po !== '') ? 'WO#' + info?.customer_po : <br></br>}
+              </Typography>
+              <Typography marginBottom={0.3} className={classes.clientTracking} align='left'>
+                {(info?.client_tracking_number && info?.client_tracking_number !== '') ? t('locations.work_orders.tracking') + '#' + info?.client_tracking_number : <br></br>}
+              </Typography>
+              <Typography marginBottom={0.3} className={classes.woType} align='left'>
+                {(info?.call_type && info?.call_type !== '') ? info?.call_type : <br></br>}
+              </Typography>
+              <Typography marginBottom={0.3} className={classes.startLabel} align='left'>
+                {(info?.open_date && info?.open_date !== '') ? t('locations.work_orders.start') + (info?.open_date) ? moment.unix(info.open_date).format('DD/MM/YY hh:mm a') : '' : <br></br>}
+              </Typography>
+              <Typography marginBottom={0.3} className={classes.woType} align='left'>
+                {getServicesLabel() !== '' ? t('locations.work_orders.service_details') + ':' + getServicesLabel() : ''}
+              </Typography>
             </Box>
             <Box flex={3} pr={1}>
               <Typography marginBottom={0.3} className={classes.woStatus} align='left'>
